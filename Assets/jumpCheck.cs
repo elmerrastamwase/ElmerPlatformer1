ed7651a6acf5531af3f8fecb83e70c0aa4eba037
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2018-11-09 16:02:48 +0100

    initial and basics

---
+++ Assets/jumpCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpCheck : MonoBehaviour {
+
+    public bool isgrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isgrounded = true; 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isgrounded = false;
+    }
+}
---
+++ Assets/walk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour
+{
+
+    public float moveSpeed = 6;
+    public float jumpSpeed = 12;
+    public jumpCheck check;
+
+    private Rigidbody2D rbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbody.velocity = new Vector2(
+            Input.GetAxisRaw("Horizontal") * moveSpeed, 
+            rbody.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (check.isgrounded == true)
+            {
+                rbody.velocity = new Vector2(rbody.velocity.x, jumpSpeed);
+            }
+        }
+
+    }
+}
